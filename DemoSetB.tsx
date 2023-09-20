17e4d877ff18f5989a6f5a9a087d73267c9079de
Jonathan Branam <jonathan.branam@gmail.com>
2023-09-20 00:17:47 -0400

    Timer demos.

--- DemoSetB.tsx
+++ DemoSetB.tsx
@@ -4,6 +4,7 @@ import styles from "./styles";
 import SetBDemo1 from "./SetBDemo1";
 import SetBDemo2 from "./SetBDemo2";
 import SetBDemo3 from "./SetBDemo3";
+import SetBDemo4 from "./SetBDemo4";
 
 const Tab = createBottomTabNavigator();
 export default function DemoSetB() {
@@ -17,6 +18,7 @@ export default function DemoSetB() {
       <Tab.Screen name="Demo 1" component={SetBDemo1} />
       <Tab.Screen name="Demo 2" component={SetBDemo2} />
       <Tab.Screen name="Demo 3" component={SetBDemo3} />
+      <Tab.Screen name="Demo 4" component={SetBDemo4} />
     </Tab.Navigator>
   );
 }
